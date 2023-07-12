922c017d945593bb9c00e205f6ffedf6ef5f88d0
Preet bhardwaj <preetb059@gmail.com>
2023-07-12 15:34:57 +0530

    final commit

---
+++ src/data/ProjectData.js
@@ -0,0 +1,83 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+    {
+      img: "https://github.com/LitPreet/Interactive-card-details-form/raw/master/design/desktop-preview.jpg",
+      title: "Deno",
+      description: "Frontend mentor challenge, Deno is an interactive form.",
+      tech_stack: "Html, Css, Javascript",
+      github_url: "https://github.com/LitPreet/Interactive-card-details-form",
+      demo_url: "https://interactive-form-98.netlify.app/",
+    },
+    {
+      img: "https://user-images.githubusercontent.com/77205201/252887198-d20dba35-c6a5-4a1e-b951-4118529c1ac9.png",
+      title: "Vim",
+      description:
+        "An online portfolio generator. Great to practice flex/grid layouts, absolute positioning and sliders.",
+      tech_stack: "Html, Css, Javascript",
+      github_url: "https://github.com/LitPreet/VIM",
+      demo_url: "https://vimcosmo.netlify.app/",
+    },
+    {
+      img: "https://user-images.githubusercontent.com/77205201/252886480-e39531c9-07ea-427a-980b-4c4630c0f198.png",
+      title: "Weather App",
+      description: "Weather App that fetches real time weather data",
+      tech_stack: "Html, Css, Javascript",
+      github_url: "https://github.com/LitPreet/Weather-app",
+      demo_url: "https://weather-app-98.netlify.app/",
+    },
+    {
+      img: "https://user-images.githubusercontent.com/77205201/252888036-6f7b3182-6431-4c81-8a3e-1f0025f97266.png",
+      title: "Swiggy Clone ",
+      description:
+        "This is a Swiggy clone app built using React, Redux, Vanilla CSS, and React Router DOM.",
+      tech_stack: "React js ,Redux, Css",
+      github_url: "https://github.com/LitPreet/food-app",
+      demo_url: "https://food-app-wiqq-d6mywz9pc-litpreet.vercel.app/",
+    },
+    {
+      img: "https://user-images.githubusercontent.com/77205201/252889883-4d61922c-1362-4554-8035-049b42af1514.png",
+      title: "Redesign Youtube",
+      description:
+        "Created Youtube using react and firebase that provides authentication , like and subscribe videos features ",
+      tech_stack: "Reactjs, Css, Firebase",
+      github_url: "https://github.com/LitPreet/Redesign_YOUTUBE",
+      demo_url: "https://redesign-youtube.vercel.app/",
+    },
+  ];
+  
+  // --------------------------------------- Skills
+  
+  export const stackList = [
+    {
+      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
+      name: "HTML",
+    },
+    {
+      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
+      name: "CSS",
+    },
+    {
+      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+      name: "JavaScript",
+    },
+    {
+      img: "https://camo.githubusercontent.com/2519c0d24b4bb0eb1c72f2c3281b7283a67107c812976a2fe4177770b47e7a5b/68747470733a2f2f63646e342e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6c6f676f732d332f3630302f52656163742e6a735f6c6f676f2d313032342e706e67",
+      name: "ReactJS",
+    },
+    {
+      img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg",
+      name: "Bootstrap",
+    },
+    {
+      img: "https://camo.githubusercontent.com/5734d0669fe22ce04a1cb989a156cd32c379875f6bca56d5210c9432824856d9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667",
+      name: "Tailwind Css",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+      name: "Git",
+    },
+    {
+      img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+      name: "Responsive",
+    },
+  ];
